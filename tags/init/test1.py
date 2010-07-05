0f3b3deb5bb7f2d466674a14607e65629e87c8f3
nobody <nobody@c4be2432-65ca-11e0-b4af-cfd3c9940da5>
2010-07-05 19:03:20 +0000

    This commit was manufactured by cvs2svn to create tag 'init'.

---
+++ tags/init/test1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# $Id: test1.py,v 1.1.1.1 2010-07-05 19:03:19 oops Exp $
+
+import sys
+import os
+import urllib
+
+try:
+	import chardet
+except ImportError:
+	sys.path.append (os.getcwd() + '/build/lib.linux-i686-2.5')
+	import chardet
+
+urlread = lambda url: urllib.urlopen (url).read ()
+
+urls = [
+	'http://google.cn',
+	'http://yahoo.co.jp',
+	'http://amazon.co.jp',
+	'http://pravda.ru',
+	'http://auction.co.kr',
+	'http://haaretz.co.il'
+]
+
+for url in urls :
+	err = []
+	print "URL %-20s : " % url,
+	# det member => encoding(string), confidence(.2f)
+	det = chardet.detector (urlread (url), err)
+	if ( det == None ) :
+		print "Error: %s" % err
+	print "encoding: %-15s, confidence: %.2f" % (det.encoding, det.confidence)
+
+sys.exit (0)
