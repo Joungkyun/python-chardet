776a84a1d35176d5d36264b2fe28244318641e69
JoungKyun Kim <joungkyun@daum.net>
2010-07-05 19:03:19 +0000

    initialize

---
+++ version.h
@@ -0,0 +1,15 @@
+/*
+ * $Id: version.h,v 1.1.1.1 2010-07-05 19:03:19 oops Exp $
+ */
+
+#define MOD_VERSION "1.0.0"
+#define MOD_UVERSION "002000000"
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
