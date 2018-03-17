c8e2501994000ad98fef9e774e7c1c25ed14fdfc
JoungKyun Kim <joungkyun@gmail.com>
2018-03-17 22:12:14 +0900

    add python 3.x classfier

--- setup.py
+++ setup.py
@@ -83,6 +83,10 @@ setup (
 			'Programming Language :: Python :: 2.6',
 			'Programming Language :: Python :: 2.7',
 			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.4',
+			'Programming Language :: Python :: 3.5',
+			'Programming Language :: Python :: 3.6',
+			'Programming Language :: Python :: 3.7',
 			'Topic :: Software Development :: Libraries :: Python Modules'
 		],
 		keywords     = [ 'encoding', 'charset', 'i18n' ]
