8b37ce711b98d6690edeb71c58692c2bfc1de97c
JoungKyun Kim <joungkyun@gmail.com>
2018-03-18 01:14:41 +0900

    add copyright in sources files

--- src/version.h
+++ src/version.h
@@ -1,3 +1,17 @@
+/*
+ * Copyfight (c) 2018 JoungKyun.Kim <hostmaster@oops.org>
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ */
+
 #define MOD_CHARDET_VERSION "2.0.1"
 #define MOD_CHARDET_UVERSION "002000001"
 #define MOD_CHARDET_LVERSION 0x02000001
